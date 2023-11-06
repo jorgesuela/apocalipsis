d07bb0ffcb967c49d2988eaead8b649c59e743ff
jorgesuela <104921157+jorgesuela@users.noreply.github.com>
2023-11-06 15:15:46 +0100

    Cambio en el metodo generarArma()
    no funcionaba correctamente, ahora funciona bien

--- src/Equipo/Arma.java
+++ src/Equipo/Arma.java
@@ -48,8 +48,8 @@ public class Arma extends Equipo {
         System.out.println("nDados: " + getNumDados());
     }
 
-    public static Arma generarArmaAleatoria(){
-        String nombreArma = "";
+    public static Arma generarArmaAleatoria() {
+        String nombreArma = "Arma";
         int potencia = 0;
         int alcance = 0;
         int valorExito = 0;
@@ -60,32 +60,58 @@ public class Arma extends Equipo {
 
         // POTENCIA DE ARMA ENCONTRADA
         int randomPotencia = random.nextInt(max - min + 1) + min;
-        if (randomPotencia < 50) potencia = 1; nombreArma = "arma mala"; //50 % potencia 1 en arma encontrada
-        if ((randomPotencia >= 50) && (randomPotencia < 90) ) potencia = 2; nombreArma = "arma estandar"; //40 % potencia 2 en arma encontrada
-        if (randomPotencia >= 90) potencia = 3; nombreArma = "arma potente"; //10 % potencia 3 en arma encontrada
-
+        if (randomPotencia < 50) {
+            potencia = 1;
+            nombreArma += " mala";
+        } else if ((randomPotencia >= 50) && (randomPotencia < 90)) {
+            potencia = 2;
+            nombreArma += " estandar";
+        } else {
+            potencia = 3;
+            nombreArma += " potente";
+        }
 
         // ALCANCE DE ARMA ENCONTRADA
         int randomAlcance = random.nextInt(max - min + 1) + min;
-        if (randomAlcance < 20) alcance = 0; nombreArma += ", cuerpo a cuerpo";//20 % alcance 0 en arma encontrada
-        if ((randomAlcance >= 20) && (randomAlcance < 50)) alcance = 1; nombreArma += ", de alcance 1"; //30 % alcance 1 en arma encontrada
-        if ((randomAlcance >= 50) && (randomAlcance < 85)) alcance = 2; nombreArma += ", de alcance 2";//35 % alcance 2 en arma encontrada
-        if (randomAlcance >= 85) alcance = 3; nombreArma += ", de alcance 3"; //15 % alcance 3 en arma encontrada
+        if (randomAlcance < 20) {
+            alcance = 0;
+            nombreArma += ", cuerpo a cuerpo";
+        } else if ((randomAlcance >= 20) && (randomAlcance < 50)) {
+            alcance = 1;
+            nombreArma += ", de corto alcance";
+        } else if ((randomAlcance >= 50) && (randomAlcance < 85)) {
+            alcance = 2;
+            nombreArma += ", de medio alcance";
+        } else {
+            alcance = 3;
+            nombreArma += ", de largo alcance";
+        }
 
         // VALOR EXITO DE ARMA ENCONTRADA
         int randomExito = random.nextInt(max - min + 1) + min;
-        if (randomExito < 40) valorExito = 4; nombreArma += ", exito con 4";//40 % exito con 4 en arma encontrada
-        if (randomExito >= 40) valorExito = 5; nombreArma += ", exito con 5"; //60 % exito con 5 en arma encontrada
+        if (randomExito < 40) {
+            valorExito = 4;
+            nombreArma += ", exito con 4";
+        } else {
+            valorExito = 5;
+            nombreArma += ", exito con 5";
+        }
 
         // NUMDADOS DE ARMA ENCONTRADA
         int randomDados = random.nextInt(max - min + 1) + min;
-        if (randomDados < 30) numDados = 1; nombreArma += " y de disparo unico";//30 % 1 dado en arma encontrada
-        if ((randomDados >= 30) && (randomDados < 85)) numDados = 2; nombreArma += " y de disparo doble"; //55 % 2 dados en arma encontrada
-        if (randomDados >= 85) numDados = 3; nombreArma += "y de disparo triple"; //15 % 3 dados en arma encontrada
+        if (randomDados < 30) {
+            numDados = 1;
+            nombreArma += " y de disparo unico";
+        } else if ((randomDados >= 30) && (randomDados < 85)) {
+            numDados = 2;
+            nombreArma += " y de disparo doble";
+        } else {
+            numDados = 3;
+            nombreArma += " y de disparo triple";
+        }
 
         Arma armaEncontrada = new Arma(nombreArma, potencia, alcance, valorExito, numDados);
 
-        //
         return armaEncontrada;
     }
 
