7e88a71bfaab2a76c84215142f038316b7febb06
jorgesuela <j.suela.2017@alumnos.urjc.es>
2023-11-11 00:24:11 +0100

    Update Main.java
    se ha modificado el main para que empiece a funcionar con una instancia de juego como debería ser

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import Equipo.Arma;
 import Equipo.Equipo;
 import Equipo.Suministro;
 import logica.Casilla;
+import logica.Juego;
 import logica.Tablero;
 
 import java.util.ArrayList;
@@ -21,32 +22,8 @@ public class Main {
         Equipo suministro = Suministro.generarSuministroAleatorio();
         suministro.mostrarInfo();
 
-        // Crear un tablero
-        Tablero tablero = new Tablero(5);
-
-// Crear casillas y agregar zombis a las casillas
-        Casilla casilla1 = tablero.getCasilla(2, 2);
-        Zombi zombi1 = new Toxico(casilla1, 1, 2);
-
-        Casilla casilla2 = tablero.getCasilla(3, 2);
-        Zombi zombi2 = new Toxico(casilla2, 1, 2);
-
-// Crear listas de zombis y supervivientes
-        ArrayList<Zombi> listaZombi = new ArrayList<>();
-        ArrayList<Superviviente> listaSup = new ArrayList<>();
-
-// Agregar los zombis a la lista de zombis
-        listaZombi.add(zombi1);
-        listaZombi.add(zombi2);
-
-// Llamar a printTablero pasando las listas de zombis y supervivientes
-        tablero.printTablero(listaZombi, listaSup);
-
-// prueba moviendo un zombie del tablero
-        zombi2.moverse(tablero);
-        tablero.printTablero(listaZombi, listaSup);
-        System.out.println(zombi2.getPosicion().toString());
-        System.out.println(tablero.hayZombi(listaZombi, 2, 2));
+        Juego juego = new Juego();
+        juego.iniciar();
     }
 }
 
